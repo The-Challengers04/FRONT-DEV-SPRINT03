1b9323d773911d54d369e35140c304c136cdfdb1
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-24 19:48:12 -0300

    update

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,61 +3,54 @@ import MenuPrincipal from "../componentes/MenuPrincipal/MenuPrincipal";
 import Titulo from "../componentes/Titulo";
 import Card from "../componentes/CardCategorias";
 import CardCupons from "../componentes/CardCupons";
-import Categorias from "../Uteis/DateCategorias";
-import Cupons from "../Uteis/DateCupons";
+import Categorias from "../Uteis/DataCategorias";
+import Cupons from "../Uteis/DataCupons";
 import styled from "styled-components";
 import Footer from "../componentes/Footer";
 import NavBar from "../componentes/NavBar";
-import Welcome from '../imagens/welcome.png';
+import Welcome from "../imagens/welcome.png";
 
+export function Home() {
+  return (
+    <div>
+      <NavBar />
+      <MenuPrincipal texto="Seja bem-vindo ao Inclui+" src={Welcome} />
 
-export function Home(){
-    return(
-        <div>
-            <NavBar/>
-            <MenuPrincipal
-                texto="Seja bem-vindo ao Inclui+"
-                src={Welcome}
-            />
+      <Titulo titulo="Busque por categorias: " />
 
-            <Titulo titulo="Busque por categorias: "/>
+      <CardGrid>
+        {Categorias.map((card, index) => (
+          <Card
+            key={index}
+            imagem={card.imagem}
+            subtitulo={card.subtitulo}
+            legenda={card.legenda}
+          />
+        ))}
+      </CardGrid>
 
-            <CardGrid>
-                {Categorias.map((card, index)=>(
-                    <Card
-                        key={index}
-                        imagem = {card.imagem}
-                        subtitulo={card.subtitulo}
-                        legenda={card.legenda}
-                    />
-                ))}
-            </CardGrid>
+      <Titulo titulo="Cupons em destaque!" />
+      <CardGrid>
+        {Cupons.map((card, index) => (
+          <CardCupons
+            key={index}
+            imagem={card.imagem}
+            subtitulo={card.subtitulo}
+            legenda={card.legenda}
+            paragrafo={card.paragrafo}
+          />
+        ))}
+      </CardGrid>
 
-            
-            <Titulo titulo="Cupons em destaque!"/>
-            <CardGrid>
-                {Cupons.map((card, index)=>(
-                    <CardCupons
-                        key={index}
-                        imagem = {card.imagem}
-                        subtitulo={card.subtitulo}
-                        legenda={card.legenda}
-                        paragrafo={card.paragrafo}
-                    />
-                ))}
-            </CardGrid>
-
-            <Footer/>
-
-        </div>
-    )
+      <Footer />
+    </div>
+  );
 }
 
 const CardGrid = styled.div`
-display: flex;
-flex-direction: row;
-flex-wrap: wrap;
-justify-content: center;
-margin: 50px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: 50px;
 `;
-
